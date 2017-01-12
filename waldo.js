9fa99bdd55e2e8c80d68d0f3093f4a5493cae9c1
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-12 17:27:01 +0000

    using forEach

--- waldo.js
+++ waldo.js
@@ -1,9 +1,14 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+  /*for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  }*/
+  arr.forEach(function(name, i){
+    if (name === 'Waldo'){
+      actionWhenFound(i);
+    }
+  });
 }
 
 function actionWhenFound(index) {
