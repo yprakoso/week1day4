57909454f1247b9bd105b718f9f649c832ab810d
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-12 19:02:42 +0000

    add addvanceSorting.js - Callbacks - Custom Sorting

---
+++ advanceSorting.js
@@ -0,0 +1,19 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+  //console.log(a.name, b.name);
+  if (a.name == b.name) {
+    if (a.age < b.age){
+      return 1;
+    }
+  } else if (a.name > b.name){
+    return 1;
+  } else return -1;
+});
+
+console.log(students);
